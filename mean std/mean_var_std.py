74f56c444ab74409856c7b11232701ff358fb19f
nathnath2006 <166334241+nathnath2006@users.noreply.github.com>
2024-04-13 14:51:40 +0200

    Update mean_var_std.py

--- mean std/mean_var_std.py
+++ mean std/mean_var_std.py
@@ -1,12 +1,29 @@
 import numpy as np
 
+import numpy as np
+
+def conform(list):
+    if len(list) != 9:
+       raise ValueError
+
+
 def calculate(list):
-    narray = np.array(list)
-    narray.reshape((3,3))
-    calculations = narray
+    try:
+      conform(list)
+      tlist = [list[0:3], list[3:6], list[6:9]]
+      narray = np.array(tlist)
+      narray.reshape((3,3))
+      trnarray = np.transpose(narray)
+      calculations ={'mean': [[np.mean(trnarray[i]) for i in range(3)], [np.mean(narray[i]) for i in range(3)], np.mean(narray)],
+                     'variance': [[np.var(trnarray[i]) for i in range(3)], [np.var(narray[i]) for i in range(3)], np.var(narray)],
+                     'standard deviation': [[np.std(trnarray[i]) for i in range(3)], [np.std(narray[i]) for i in range(3)], np.std(narray)],
+                     'max': [[np.max(trnarray[i]) for i in range(3)], [np.max(narray[i]) for i in range(3)], np.max(narray)],
+                     'min': [[np.min(trnarray[i]) for i in range(3)], [np.min(narray[i]) for i in range(3)], np.min(narray)],
+                     'sum': [[np.sum(trnarray[i]) for i in range(3)], [np.sum(narray[i]) for i in range(3)], np.sum(narray)]}
+      return calculations
+    except ValueError as ve:
+       return "List must have nine numbers"
 
 
 
-
-    return calculations
 
